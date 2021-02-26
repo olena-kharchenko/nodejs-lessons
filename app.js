07d25ee700f7515754ed3248427525413f564807
olena-kharchenko <harchenko.es1990@gmail.com>
2021-02-26 16:35:50 +0200

    add lesson 4

--- app.js
+++ app.js
@@ -1,23 +1,25 @@
 const express = require('express')
 const logger = require('morgan')
+const cors = require('cors')
+
+const catsRouter = require('./routes/api/cats')
 
 const app = express()
-const weatherRouter = require('./routes/api/weather')
 
-app.use(logger('dev'))
+const formatsLogger = app.get('env') === 'development' ? 'dev' : 'short'
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+app.use(logger(formatsLogger))
+app.use(cors())
+app.use(express.json())
+
+app.use('/api/cats', catsRouter)
+
+app.use((req, res) => {
+  res.status(404).json({ message: 'Not found' })
 })
 
-app.use('/weather', weatherRouter)
-
-app.use((_req, res) => {
-  res.status(404).send({ message: 'Not Found' })
-})
-
-app.use((err, _req, res, _next) => {
-  res.status(500).send({ message: err.message })
+app.use((err, req, res, next) => {
+  res.status(err.status || 500).json({ message: err.message })
 })
 
 module.exports = app
